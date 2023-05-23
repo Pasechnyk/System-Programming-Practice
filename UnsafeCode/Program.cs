d5a5835dc2fccded4bf76ce9becf8627ff39d3ae
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-05-23 23:48:49 +0300

    Add Unsafe Code project draft

---
+++ UnsafeCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnsafeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
